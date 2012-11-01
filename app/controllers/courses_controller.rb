cd52970eb45d05397d1f8f5be0feb7b6b6d5c85d
Pankaj More <pankajmore@gmail.com>
2012-11-01 19:13:12 +0530

    search complete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,9 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+  def show
+    search = params[:q]
+    search_condition = "%" + search + "%"
+    @courses = Course.find(:all, :conditions => ['name LIKE ? or number LIKE ?', search_condition, search_condition]) 
+  end
 end
