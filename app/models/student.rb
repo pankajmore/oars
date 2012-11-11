4695df3f2948e26e351b13e5bfe1cf359afe920b
satvikc <mystic.satvik@gmail.com>
2012-11-11 23:24:52 +0530

    dummy department added

--- app/models/student.rb
+++ app/models/student.rb
@@ -30,7 +30,7 @@ class Student < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :department_id
   attr_accessible :name, :phone, :roll, :username
   belongs_to :department
   has_many :registration_forms, :dependent => :destroy
