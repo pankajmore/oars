ff2f453d19cdecd4060e8c775f8e0e63424a0b36
satvikc <mystic.satvik@gmail.com>
2012-11-13 22:52:50 +0530

    clash function added

--- app/helpers/student/pre_registration_helper.rb
+++ app/helpers/student/pre_registration_helper.rb
@@ -1,2 +1,17 @@
 module Student::PreRegistrationHelper
+    def clash(course_to_add,form)
+        form.offered_courses.each do |r|
+            r.lecture_times.each do |l|
+                course_to_add.lecture_times.each do |c|
+                    if l.day == c.day
+                        if l.start_time >= c.start_time and l.start_time <= c.end_time 
+                            return r 
+                        elsif c.start_time >= l.start_time and c.start_time <= l.end_time 
+                            return r
+                        end 
+                    end 
+                end
+            end
+        end
+    end 
 end
