b96817f99adea567a5f674d81e03d92e8f45b1d1
rahulaaj <rahulajmera91@gmail.com>
2012-11-15 03:48:37 -0800

    test cases

--- features/step_definitions/update_info_fail.rb
+++ features/step_definitions/update_info_fail.rb
@@ -1,21 +1,22 @@
 Given /^the user visits the update personal information page$/ do
-  visit personal_info
+  visit student_personal_info_path
 end
 
 Given /^the user clicks on update link$/ do
-  click_link "Update_info"
+  visit "/students/edit"
 end
 
 When /^he submits his invalid password$/ do
-  fill_in "Name" , with: "Not Name"
-  fill_in "Password" , with: "notpassword"
+  #fill_in "student_name" , with: "Not Name"
+  #fill_in "student_current_password" , with: "notpassword"
+  click_button "Update"
 end
 
 Then /^he should see an error message$/ do
-  page.should have_selector('div.alert.alert-error')
+  page.should have_selector('div', class:'alert alert-error')
 end
 
 Then /^nothing should be updated$/ do
-  student.find("name","Not Name")
+  not Student.find_by_name("Not Name")
 end
 
